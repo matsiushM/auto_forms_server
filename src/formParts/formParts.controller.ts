ac376828fa97c9046864f38c7e33cccd3f9324b5
Maxim <matiush2018@yandex.ru>
2024-04-13 13:59:43 +0300

    Added new responce for 1c server

--- src/formParts/formParts.controller.ts
+++ src/formParts/formParts.controller.ts
@@ -42,4 +42,40 @@ export class FormPartsController {
       throw error;
     }
   }
+
+  @Post('/dataPhoto')
+  async handleDataPhoto(@Body() data: any) {
+    try {
+      console.log('Received data:', data);
+
+      const url = 'http://178.124.201.2/InfoBase/hs/Zagruzka/StoksPOST_Foto/json';
+      const authHeader = 'Basic ' + Buffer.from('111:').toString('base64');
+
+      const response: Observable<any> = this.httpService.post(url, data, {
+        headers: {
+          Authorization: authHeader,
+        },
+      }).pipe(
+          catchError(error => {
+            console.error('Error sending data:', error.response?.data || error.message);
+            throw error;
+          })
+      );
+
+      response.subscribe({
+        next: responseData => {
+          console.log('Response from external API:', responseData.data);
+        },
+        error: error => {
+          console.error('Error sending data:', error.response?.data || error.message);
+          throw error;
+        }
+      });
+
+      return { message: 'Data sent to external API successfully' };
+    } catch (error) {
+      console.error('Error sending data:', error.response?.data || error.message);
+      throw error;
+    }
+  }
 }
