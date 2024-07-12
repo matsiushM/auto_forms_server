85e4c183e8fc316c9b615b43e4b5682cd3989ccf
Maxim <matiush2018@yandex.ru>
2024-07-12 15:27:02 +0300

    added sendIdParts controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import {Module} from '@nestjs/common';
 import {FormPartsModule} from "./formParts/formParts.module";
 import { AuthorizationModule } from './authorization/authorization.module';
 import { LoadPartsModule } from './load-parts/load-parts.module';
-import {HttpModule} from "@nestjs/axios";
 
 
 @Module({
--- src/load-parts/load-parts.controller.ts
+++ src/load-parts/load-parts.controller.ts
@@ -29,4 +29,26 @@ export class LoadPartsController {
             }
         });
     }
+
+    @Post('/sendIdParts')
+    async sendIdParts(@Body() data: any, @Res() res: Response) {
+        const url = 'http://178.124.201.2/InfoBase/hs/ZaprosZapch/stoks_Zapch/json';
+        const authHeader = 'Basic ' + Buffer.from('111:').toString('base64');
+        const response: Observable<any> = this.httpService.post(url, data, {
+            headers: {
+                Authorization: authHeader,
+            },
+        });
+
+        response.subscribe({
+            next: responseData => {
+                console.log('Response from external API:', responseData.message);
+                res.status(responseData.status).send(responseData.data);
+            },
+            error: error => {
+                console.error('Error sending data:', error.response?.data || error.message);
+                res.status(error.response.status).send();
+            }
+        });
+    }
 }
