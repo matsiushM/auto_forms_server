ab7a054a2c18eafee636bcb3127717f04c6fc70a
Maxim <matiush2018@yandex.ru>
2024-11-04 13:59:58 +0300

    edited api for login

--- src/authorization/authorization.controller.ts
+++ src/authorization/authorization.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Post, Res} from '@nestjs/common';
+import {Body, Controller, Post, Res} from '@nestjs/common';
 import {HttpService} from "@nestjs/axios";
 import {Observable} from "rxjs";
 import { Response } from 'express';
@@ -8,8 +8,8 @@ export class AuthorizationController {
     constructor(private httpService: HttpService) {
     }
 
-    @Post('/login')
-    async login(@Res() res: Response) {
+    @Post('/loadLogin')
+    async loadLogin(@Res() res: Response) {
         const data = JSON.stringify( {test: "dghsghd"});
         console.log(data)
         const url = 'http://178.124.201.2/InfoBase/hs/ZaprosUsers/stoks_Users/json';
@@ -23,6 +23,28 @@ export class AuthorizationController {
         response.subscribe({
             next: responseData => {
                 console.log('Response from external API:', responseData.message);
+                res.status(responseData.status).send(responseData.data);
+            },
+            error: error => {
+                console.error('Error sending data:', error.response?.data || error.message);
+                res.status(error.response.status).send();
+            }
+        });
+    }
+
+    @Post('/login')
+    async login(@Res() res: Response,@Body() data: any) {
+        const url = 'http://178.124.201.2/InfoBase/hs/ZaprosUsersAnswer/stoks_UsersAnswer/json';
+        const authHeader = 'Basic ' + Buffer.from('111:').toString('base64');
+        const response: Observable<any> = this.httpService.post(url, data, {
+            headers: {
+                Authorization: authHeader,
+            },
+        });
+
+        response.subscribe({
+            next: responseData => {
+                console.log('Response from external API:', responseData.data);
                 res.status(responseData.status).send(responseData.data);
             },
             error: error => {
